dacee3740e6f6f7c5dad86ac8903af397a391fef
Karim Alibhai <karim@alibhai.co>
2019-09-08 03:07:45 -0400

    Remove: redirect after 401

--- packages/web/src/models/axios.js
+++ packages/web/src/models/axios.js
@@ -44,13 +44,14 @@ export function removeAuthToken() {
 
 export const data = fn => (...args) => fn(...args).then(d => d.data)
 
-export const apiHost = ({
+export const apiHost = {
 	// TODO: Add production & staging URLs here
 
 	local: location.hostname,
-})[process.env.ENV_TARGET || 'local']
+}[process.env.ENV_TARGET || 'local']
 
-export const apiPort = (process.env.ENV_TARGET || 'local') === 'local' ? 8080 : 443
+export const apiPort =
+	(process.env.ENV_TARGET || 'local') === 'local' ? 8080 : 443
 export const useSecureAPI = process.env.NODE_ENV === 'production'
 
 export const axios = axiosMod.create({
@@ -65,38 +66,58 @@ axios.interceptors.request.use(req => {
 	return req
 })
 
-axios.interceptors.response.use(res => res, error => {
-	// For non-login routes, we should automatically detect when the user is logged out
-	// and eject them back to the login screen
-	if (
-		error.config &&
-		!error.config.url.endsWith('/users/login') &&
-		!error.config.url.endsWith('/users/current') &&
-		error.response &&
-		error.response.status === 401
-	) {
-		console.warn('Forcing redirect to /login')
-		removeAuthToken()
-		location.href = '/login'
-		return
-	}
-
-	if (String(error).match(/network error/i) || (error.response && error.response.data && error.response.data.error && error.response.status >= 500)) {
-		return Promise.reject(new Error('The application is currently unavailable. Please try again later.'))
-	}
-
-	// For errors in which the backend has intentionally sent a message, we should rewrite
-	// the error object so the UI can only be concerned with the UI bits
-	if (error.response && error.response.data && error.response.data.error && error.response.status < 500) {
-		if (error.response.data.displayMessage) {
-			console.warn(`Rewriting error message:`, error.response.data)
+axios.interceptors.response.use(
+	res => res,
+	error => {
+		// For non-login routes, we should automatically detect when the user is logged out
+		// and eject them back to the login screen
+		if (
+			error.config &&
+			!error.config.url.endsWith('/users/login') &&
+			!error.config.url.endsWith('/users/current') &&
+			error.response &&
+			error.response.status === 401
+		) {
+			removeAuthToken()
+			return
 		}
-		const newError = new Error(error.response.data.displayMessage ? error.response.data.error : 'Something went wrong. Please try again later.')
-		newError.response = error.response
-		return Promise.reject(newError)
-	}
 
-	// For all other errors (such as network errors), we should simply
-	// return the error as-is
-	return Promise.reject(error)
-})
+		if (
+			String(error).match(/network error/i) ||
+			(error.response &&
+				error.response.data &&
+				error.response.data.error &&
+				error.response.status >= 500)
+		) {
+			return Promise.reject(
+				new Error(
+					'The application is currently unavailable. Please try again later.',
+				),
+			)
+		}
+
+		// For errors in which the backend has intentionally sent a message, we should rewrite
+		// the error object so the UI can only be concerned with the UI bits
+		if (
+			error.response &&
+			error.response.data &&
+			error.response.data.error &&
+			error.response.status < 500
+		) {
+			if (error.response.data.displayMessage) {
+				console.warn(`Rewriting error message:`, error.response.data)
+			}
+			const newError = new Error(
+				error.response.data.displayMessage
+					? error.response.data.error
+					: 'Something went wrong. Please try again later.',
+			)
+			newError.response = error.response
+			return Promise.reject(newError)
+		}
+
+		// For all other errors (such as network errors), we should simply
+		// return the error as-is
+		return Promise.reject(error)
+	},
+)
